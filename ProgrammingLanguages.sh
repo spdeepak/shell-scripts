ab562a97d44af6383b402602a4e6a65c6fab8f6d
Deepak Sunanda Prabhakar <spdeepak@users.noreply.github.com>
2016-10-02 15:33:50 +0100

    Create ProgrammingLanguages.sh
    Added Java, Scala installation command

---
+++ ProgrammingLanguages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "================================="
+echo "== Adding Java to install list =="
+echo "================================="
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+echo "====================="
+echo "== Installing Java8=="
+echo "====================="
+sudo apt-get install oracle-java8-installer
+echo "====================="
+echo "== Installing Java7=="
+echo "====================="
+sudo apt-get install oracle-java7-installer
+echo "======================"
+echo "== Installing Java6 =="
+echo "======================"
+sudo apt-get install oracle-java6-installer
+echo "=============================="
+echo "== Setting Java8 as default =="
+echo "=============================="
+sudo apt-get install oracle-java8-set-default
+echo "============================="
+echo "== Downloading Scala 2.11.8=="
+echo "============================="
+sudo wget "http://www.scala-lang.org/files/archive/scala-2.11.8.deb"
+sudo dpkg -i scala-2.11.8.deb
+sudo rm -r scala-2.11.8.deb
